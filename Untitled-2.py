f83247ec505e32d4879898c3b56d4a835805ea21
Dawid Dymke <dawid584@yahoo.com>
2022-08-23 05:50:07 +0200

    Add corrected

--- Untitled-2.py
+++ Untitled-2.py
@@ -1,19 +1,19 @@
 word = input("enter your word:  ")
-word = word.lower()
+
 # wprowadzanie wyrazu
-def palindome(word):
-    if (word == word[::-1]):
+def palindrom(word):
+    if word =="".join(reversed(word)):
+     #    Funkcja sprawdzająca czy dany wyraz to palindrom   
         return True
     else:
         return False
-        """"
-        Funkcja sprawdzająca czy dany wyraz to palndrom
-        """
-if (palindome(word) == True):
-    print("this word is palindome")
-else:    
-    print("this word is not palindome")
+        
+if (palindrom(word) == True):
     # Wyświetlanie informacji dla sprawdzającego czy dany wyraz to palindrom
 
+    print("this word is palindrom")
+else:    
+    print("this word is not palindrom")
+    
     
     
